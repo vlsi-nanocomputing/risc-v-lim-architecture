8158265f20f1b78809525cd3ca40dbbca0f7c538
Gianluca Goti <gianluca.goti@studenti.polito.it>
2022-07-09 14:35:11 +0200

    Added comment to bitwise_inv.c

--- RI5CY-sameIF/tb/core/custom/bitwise_inv.c
+++ RI5CY-sameIF/tb/core/custom/bitwise_inv.c
@@ -1,40 +1,46 @@
+/*CUSTOM PROGRAM*/
+//Test built-in logic functions (NAND-NOR-XNOR)
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
 {    
     /* variable declaration */
-    int N = 5, i, mask_or, mask_and, mask_xor;
-    int *vector = 0x030000, *stand_alone = 0x30040, *final_result = 0x30080;
+    int N = 5, i, mask_nor, mask_nand, mask_xnor;  //define useful variables
+    int *vector = 0x030000, *stand_alone = 0x30040, *final_result = 0x30080;    //define memory addresses
 
     /* fill vector */
     for(i=0; i<N; i++){    
     	vector[i] = i*13467;
     }
+    //initialize stand alone value
     *stand_alone = vector[1]+0x768;
 
-    /* OR operation */
-    mask_or = 0xF1;
+    /* NOR operation */
+    mask_nor = 0xF1; //NOR mask definition
     for(i=0; i<N; i++){
-    	vector[i] = ~(vector[i] | mask_or);
+    	vector[i] = ~(vector[i] | mask_nor);    //compute NOR operation on each vector element
     }
-    *stand_alone = ~(*stand_alone | mask_or);
+    *stand_alone = ~(*stand_alone | mask_nor);  //compute NOR on stand alone element
 
-    /* AND operation */
-    mask_and = ~(vector[N-1] & 0x8F);
+    /* NAND operation */
+    mask_and = ~(vector[N-1] & 0x8F);   //NAND mask computation
     for(i=0; i<N; i++){
-    	vector[i] = ~(vector[i] & mask_and);
+    	vector[i] = ~(vector[i] & mask_and);    //compute NAND on each vector element
     }
-    *stand_alone = ~(*stand_alone & mask_and);
+    *stand_alone = ~(*stand_alone & mask_and);  //compute NAND on stand alone element
   
-    /* XOR operation */
-    mask_xor = ~(vector[N-2] ^ 0xF0);
+    /* XNOR operation */
+    mask_xor = ~(vector[N-2] ^ 0xF0);   //XNOR mask computation
     for(i=0; i<N; i++){
-    	vector[i] = ~(vector[i] ^ mask_xor);
+    	vector[i] = ~(vector[i] ^ mask_xor);    //compute XNOR on each vector element
     }
-    *stand_alone = ~(*stand_alone ^ mask_xor);
+    *stand_alone = ~(*stand_alone ^ mask_xor);  //compute XNOR on stand alone element
 
-    *final_result = ~vector[N-3] + ~(*stand_alone);
+    *final_result = ~vector[N-3] + ~(*stand_alone); //compute final result
      
     return EXIT_SUCCESS;
 }
